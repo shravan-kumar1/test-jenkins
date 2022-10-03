a9d44d3079820dc39e2fd55a2564f2b2b49b5bc1
shravankumar <ch.shravan403@gmail.com>
2022-10-03 19:49:58 +0530

    initial commit

---
+++ src/test/java/sampletestcases/LoginTests.java
@@ -0,0 +1,34 @@
+package sampletestcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+	@Test
+	public void verifyloginwithValidCredentials() {
+		
+		System.out.println("Test starting...");
+		System.out.println("verifyloginwithValidCredentials");
+		Assert.assertTrue(true);
+		System.out.println("Test Ended....");
+	}
+	
+	@Test
+     public void verifyloginwithInValidCredentials() {
+		
+		 System.out.println("Test starting...");
+		 System.out.println("verifyloginwithInValidCredentials");
+		 Assert.assertTrue(true);
+		 System.out.println("Test Ended....");
+	}
+	
+      @Test
+      public void verifyloginwithNoValidCredentials() {
+	
+	      System.out.println("Test starting...");
+	      System.out.println("verifyloginwithNoCredentials");
+	      Assert.assertTrue(true);
+	      System.out.println("Test Ended....");
+}
+
+}
